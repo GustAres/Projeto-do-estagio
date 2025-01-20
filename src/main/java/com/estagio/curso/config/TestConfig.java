daf8ac0c8052329430c9d9a0fc56dcedfce374c7
Gustavo Procopio <rattesprocopio@gmail.com>
2025-01-20 17:30:49 -0300

    Associação muitos-para-muitos com JoinTable

--- src/main/java/com/estagio/curso/config/TestConfig.java
+++ src/main/java/com/estagio/curso/config/TestConfig.java
@@ -50,6 +50,14 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         user u1 = new user(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         user u2 = new user(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
