2f7b912441eb23e62c2dc6c3ba56866ab214c8d3
Gustavo Procopio <rattesprocopio@gmail.com>
2025-01-21 16:14:44 -0300

    OrderItem

--- src/main/java/com/estagio/curso/config/TestConfig.java
+++ src/main/java/com/estagio/curso/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.estagio.curso.config;
 
-import com.estagio.curso.entities.Category;
-import com.estagio.curso.entities.Order;
-import com.estagio.curso.entities.Product;
+import com.estagio.curso.entities.*;
 import com.estagio.curso.entities.enums.OrderStatus;
-import com.estagio.curso.entities.user;
-import com.estagio.curso.repositories.CategoryRepository;
-import com.estagio.curso.repositories.OrderRepository;
-import com.estagio.curso.repositories.ProductRepository;
-import com.estagio.curso.repositories.UserRepository;
+import com.estagio.curso.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
 
     @Override
@@ -67,6 +64,13 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 
 
